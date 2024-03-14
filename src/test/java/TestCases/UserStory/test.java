25dcc6d22dbc63884e605cd34b3ebed81b565de7
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2024-03-14 19:20:46 +0530

    13march

---
+++ src/test/java/TestCases/UserStory/test.java
@@ -0,0 +1,12 @@
+package TestCases.UserStory;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Arise academy");
+    }
+}
